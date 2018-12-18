a9c159a76c4e832bb1ae7ac1a8b4cf7a48a7e387
Tim Vaneker <timvaneker@outlook.com>
2018-12-18 22:34:13 +0100

    Commented out code responsible for giving errors

--- src/server.js
+++ src/server.js
@@ -34,10 +34,10 @@ server.applyMiddleware({ app });
 // Allow cross-origin resource sharing
 app.use(cors());
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+// app.use(express.static('public'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// });
 
 // Start the server
 const PORT = process.env.PORT || 5000;
